97ab617812dbebadc1f487ea52cb446e05215d3b
Tim Miner <poeminminer1@gmail.com>
2022-06-18 12:09:12 -0500

    added class names to the inputs and save inputs as variables in monthlyPayroll function

--- client.js
+++ client.js
@@ -2,4 +2,19 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery and DOM working');
+    $('.submit').on('click', monthlyPayroll);
+}
+let sum = 0;
+
+function monthlyPayroll() {
+    let firstName = $('.fname').val();
+    console.log(firstName);
+    let lastName = $('.lname').val();
+    console.log(lastName);
+    let id = $('.id').val();
+    console.log(id);
+    let job = $('.job-title').val();
+    console.log(job);
+    let salary = $('.salary').val();
+    console.log(salary);
 }
