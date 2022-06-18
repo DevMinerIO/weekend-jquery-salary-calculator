75608d52f6caf5effd2717e338d9bb4c98b60150
Tim Miner <poeminminer1@gmail.com>
2022-06-18 17:33:48 -0500

    fixed onReady function selector so the delete button works properly.

--- client.js
+++ client.js
@@ -3,36 +3,44 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jQuery and DOM working');
     $('.submit').on('click', monthlyPayroll);
+    $('.employee-container').on('click', '.toDelete', handleDelete);
+    // .employee-container .toDelete .employee-row
 }
 let sum = 0;
 
 function monthlyPayroll() {
     let firstName = $('.fname').val();
-    console.log(firstName);
+    //console.log(firstName);
     let lastName = $('.lname').val();
-    console.log(lastName);
+    //console.log(lastName);
     let id = $('.id').val();
-    console.log(id);
+    //console.log(id);
     let job = $('.job-title').val();
-    console.log(job);
+    //console.log(job);
     let salary = Number($('.salary').val());
-    console.log(salary);
+    //console.log(salary);
 
-$('.employee-container').append(`
-<tr>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    <td>${id}</td>
-    <td>${job}</td>
-    <td>${salary}</td>
-    <td><button type="button" class="toDelete">Delete</button></td>
-</tr>`)
+    $('.employee-container').append(`
+    <tr class="employee-row">
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${id}</td>
+        <td>${job}</td>
+        <td>${salary}</td>
+        <td><button type="button" class="toDelete">Delete</button></td>
+    </tr>`);
 
-sum += salary;
-//$('.monthly-result').text(sum);
-$('.monthly-result').empty().append(sum);
-if( sum > 20000) {
-    $(".monthly-result").css("background-color", "red");
+    sum += salary;
+    //$('.monthly-result').text(sum);
+    $('.monthly-result').empty().append(sum);
+    if( sum > 20000) {
+        $(".monthly-result").css("background-color", "red");
+    }
 }
+function handleDelete() {
+    //parents function also works if you use the .employee-row in the argument to match it. 
+    //$(this).parents('.employee-row').remove();
+    $(this).closest('.employee-row').remove();
+    //$(this).closest('.employee-row').;
 }
 
