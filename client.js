b4c0a15eaac3f971f1d510aad99908502bba7d30
Tim Miner <poeminminer1@gmail.com>
2022-06-18 13:15:04 -0500

    DOM now shows the monthly total

--- client.js
+++ client.js
@@ -15,6 +15,20 @@ function monthlyPayroll() {
     console.log(id);
     let job = $('.job-title').val();
     console.log(job);
-    let salary = $('.salary').val();
+    let salary = Number($('.salary').val());
     console.log(salary);
+
+$('.employee-container').append(`
+<tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${id}</td>
+    <td>${job}</td>
+    <td>${salary}</td>
+    <td><button type="button" class="toDelete">Delete</button></td>
+</tr>`)
+
+sum += salary;
+$('.monthly-result').text(sum);
+//$('.monthly-result').empty().append(sum);
 }
